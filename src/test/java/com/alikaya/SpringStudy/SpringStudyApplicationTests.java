7acf8f68e64e1ecb40969787c848d7a4c2acba20
Ali Kaya <alikaya@mail.com>
2024-09-18 18:48:42 +0300

    first commit

---
+++ src/test/java/com/alikaya/SpringStudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alikaya.SpringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
